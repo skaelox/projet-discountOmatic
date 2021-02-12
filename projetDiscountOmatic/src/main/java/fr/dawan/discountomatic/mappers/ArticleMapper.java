248eb5fadd514b82ae4247e5f9db7a1b951d9001
Hugo Desanti <desanti.hugo@gmail.com>
2021-02-12 16:39:56 +0100

    Ajout de ArticleMapper

---
+++ projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/mappers/ArticleMapper.java
@@ -0,0 +1,5 @@
+package fr.dawan.discountomatic.mappers;
+
+public class ArticleMapper {
+
+}
--- projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/repositories/ArticleRepository.java
+++ projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/repositories/ArticleRepository.java
@@ -1,6 +1,10 @@
 package fr.dawan.discountomatic.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.dawan.discountomatic.beans.Article;
@@ -8,5 +12,6 @@ import fr.dawan.discountomatic.beans.Article;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-	
+	@Query(value = "FROM Article a WHERE a.name LIKE :search")
+    List<Article> findAllByName(@Param("search") String searchName);
 }
