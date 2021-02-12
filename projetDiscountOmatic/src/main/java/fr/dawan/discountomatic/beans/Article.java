37781c70c084532821ef2d2c75dd40ed97223d2e
Hugo Desanti <desanti.hugo@gmail.com>
2021-02-12 12:22:10 +0100

    Implementation des beans Article et Category

--- projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/beans/Article.java
+++ projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/beans/Article.java
@@ -1,14 +1,97 @@
 package fr.dawan.discountomatic.beans;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 
 @Entity
 public class Article {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long article_id;
+	@Column(name = "article_id")
+	private long articleId;
+	@Column(name = "article_name")
+	private String name;
+	@Column(name = "article_description")
+	private String description;
+	@Column(name = "article_price")
+	private double price;
+	@Column(name = "article_quantity")
+	private int quantity;
+	@Column(name = "article_visibility")
+	private boolean visibility;
+	
+	@ManyToMany(mappedBy = "articles", cascade = CascadeType.ALL)
+	@Column(name = "article_category")
+	private List<Category> categories;
+	
+	public Article() {
+		
+	}
+	
+	public Article(long articleId, String name, String description, double price, int quantity, boolean visibility,
+			List<Category> categories) {
+		super();
+		this.articleId = articleId;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.quantity = quantity;
+		this.visibility = visibility;
+		this.categories = categories;
+	}
+	public long getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(long articleId) {
+		this.articleId = articleId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public boolean isVisibility() {
+		return visibility;
+	}
+	public void setVisibility(boolean visibility) {
+		this.visibility = visibility;
+	}
+	public List<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	
+	
+	
 }
--- projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/beans/Category.java
+++ projetDiscountOmatic/src/main/java/fr/dawan/discountomatic/beans/Category.java
@@ -1,5 +1,73 @@
 package fr.dawan.discountomatic.beans;
 
-public class Category {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long customeId;
+    @Column(name = "customer_name")
+    private String name;
+    @Column(name = "customer_description")
+    private String description;
+    
+    @ManyToMany
+    private List<Article> articles;
+    
+    public Category() {
+    	
+    }
+
+	public Category(long customeId, String name, String description, List<Article> articles) {
+		this.customeId = customeId;
+		this.name = name;
+		this.description = description;
+		this.articles = articles;
+	}
+
+	public long getCustomeId() {
+		return customeId;
+	}
+
+	public void setCustomeId(long customeId) {
+		this.customeId = customeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+	
+	 
+
+   
 }
